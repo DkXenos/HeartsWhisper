0816035c2841ca2274e7d9afd909531def13b0a9
DkXenos <superjason101010@gmail.com>
2025-10-19 19:19:21 +0700

    latest

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}" />
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}" />
     <link rel="manifest" href="{{ asset('site.webmanifest') }}">
-    @vite(['resources/css/site.css', 'resources/js/app.js'])
+    @vite(entrypoints: ['resources/css/site.css', 'resources/js/app.js'])
 </head>
 <body>
     {{-- @include('components.header') --}}
